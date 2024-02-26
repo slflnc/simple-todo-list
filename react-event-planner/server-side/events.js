e6fe3029e5d8179dd79684c279ef85546f92b9e4
Alfie Lancaster <147993400+slflnc@users.noreply.github.com>
2024-02-26 09:42:33 +0000

    Delete react-event-planner/server-side directory

--- react-event-planner/server-side/events.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const eventsController = require('../controllers/eventsController');
-
-router.get('/', eventsController.getAllEvents);
-
-module.exports = router;
